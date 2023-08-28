44aa41981a45fe09fc4e8eedd17eb46b2f779735
forRicky <143424414+forRicky@users.noreply.github.com>
2023-08-28 16:15:10 +0100

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,19 +1,24 @@
-from django.urls import include, re_path
+
+from django.urls import include, re_path, path
 
 
 from .views import *
+from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
+
+    path('profile/', views.view_profile, name='view_profile'),
+
     re_path(r'^login/$', login_view, name='login'),
     re_path(r'^register/$', register_view, name='register'),
     re_path(r'^logout/$', logout_view, name='logout'),
     re_path(r'^select_user/$', select_user, name='select_user'),
-    re_path(r'^user/invest-now/$', invest_now, name='invest_now'),
-    re_path(r'^schema$', schema, name='schema'),
-    re_path(r'^investment_history$', investment_history, name='investment_history'),
-    re_path(r'^update_profile$', update_profile, name='update_profile'),
+    re_path(r'^airline/$', airline, name='airline'),
+    re_path(r'^login_history/$', login_history, name='login_history'),
+    re_path(r'^change-password/$', change_password_view, name='change_password'),
     re_path(r'^login_con$', login_con, name='login_con'),
     re_path(r'^useremail$', useremail, name='useremail'),
 ]
+
